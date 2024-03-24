dc66a5c34455ae5d7e6f3a97f4735af4e6f0ca5f
LucileDT <lucile.decrozant.triquenaux@protonmail.com>
2024-03-24 13:56:46 +0100

    Ref #4: install webpack

---
+++ assets/app.js
@@ -0,0 +1,8 @@
+/*
+ * Main JavaScript file
+ *
+ * Included in templates/base.html.twig
+ */
+
+// main CSS file, included in templates/base.html.twig too
+import './styles/app.css';
--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
